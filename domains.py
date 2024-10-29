5f5da22ce921acad87467d68d0353014185221b0
Nicolas Barrier <nicolas.barrier@ird.fr>
2024-10-29 14:13:10 +0100

    Remove unused title attr

--- domains.py
+++ domains.py
@@ -3,14 +3,12 @@ domains = {}
 
 domains['BOB'] = {
     'map': 'maps/bob_mask.nc',
-    'title': "Bay of Biscay",
     'popup': "html/template.html",
     'status': 'dev'
 }
 
 domains['BALTIC'] = {
     'map' : 'maps/med_mask.nc',
-    'title': 'Baltic Sea',
     'popup': 'html/baltic_sea.html',
     'config': 'csv/baltic_sea.csv'
 }
@@ -24,51 +22,43 @@ domains['BALTIC'] = {
 
 domains['NS'] = {
     'map': 'maps/ns_mask.nc',
-    'title': 'NS',
     'popup': 'html/ns.html',
     'config': 'csv/ns_config.csv'
 }
 
 domains['EEC'] = {
-    'title': 'EEC',
     'map': 'maps/eec_mask.nc',
     'popup': 'html/eec.html'
 }
 
 domains['GOG'] = {
     'map': 'maps/gog_mask.nc',
-    'title': 'GOG',
     'popup': 'html/gog.html'
 }
 
 domains['GOL'] = {
     'map': 'maps/gol_mask.nc',
-    'title': 'GOL',
     'popup': 'html/gol.html'
 }
 
 domains['BEN'] = {
     'map': 'maps/ben_mask.nc',
-    'title': 'BEN',
     'popup': 'html/ben.html'
 }
 
 domains['NSB'] = {
     'map': 'maps/nsb_mask.nc',
-    'title': 'NSB',
     'popup': 'html/nsb.html'
 }
 
 
 domains['BS'] = {
     'map': 'maps/bs_mask.nc',
-    'title': 'BS',
     'popup': 'html/bs.html',
     'config': 'csv/bs_config.csv'
 }
 
 domains['WAP'] = {
     'map': 'maps/wap_mask.nc',
-    'title': 'WAP',
-    'popup': 'html/bs.html'
+    'popup': 'html/wap.html'
 }
